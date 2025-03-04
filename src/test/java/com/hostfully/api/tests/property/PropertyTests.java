29316dda244006cef6e693a781b62dc638b98b88
Fernando Rodrigues <fernando.rodrigues@trustly.com>
2025-03-04 18:12:18 -0300

    property mandatory attribute tests added

--- src/test/java/com/hostfully/api/tests/property/PropertyTests.java
+++ src/test/java/com/hostfully/api/tests/property/PropertyTests.java
@@ -1,4 +1,4 @@
-package com.hostfully.api.tests;
+package com.hostfully.api.tests.property;
 
 import com.hostfully.api.config.BaseTest;
 import io.restassured.module.jsv.JsonSchemaValidator;
@@ -10,11 +10,15 @@ import java.io.IOException;
 import static com.hostfully.api.utils.FileUtils.readJsonFile;
 import static io.restassured.RestAssured.given;
 import static java.util.Optional.empty;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
-public class PropertiesTests extends BaseTest {
+public class PropertyTests extends BaseTest {
 
     private final String GET_PROPERTIES_ENDPOINT = "/properties";
+    private final String POST_PROPERTIES_ENDPOINT = "/properties";
+
+    //GET Properties endpoint
 
     @Test
     @DisplayName("GET /properties unauthorized access")
@@ -44,8 +48,44 @@ public class PropertiesTests extends BaseTest {
             .body(JsonSchemaValidator.matchesJsonSchema(readJsonFile("src/test/resources/schemas/PropertiesDTO.json")));
     }
 
-    //POST PROPERTIES: MISSING MANDATORY ATTRIBUTES
-    //POST PROPERTIES: INVALID MANDATORY ATTRIBUTES
+    //POST Properties endpoint
+
+    @Test
+    @DisplayName("POST /properties requires mandatory attributes")
+    public void testCreatePropertyRequiresMandatoryAttributes() throws IOException {
+        given()
+            .auth()
+            .preemptive()
+            .basic(username, password)
+            .body(readJsonFile("src/test/resources/requests/PropertyCreationMissingAlias.json"))
+        .when()
+            .post(POST_PROPERTIES_ENDPOINT)
+        .then()
+            .statusCode(400)
+            .body("title", is("Validation Error"))
+            .body("detail", is("Validation failed"))
+            .body("errors[0].field", is("alias"))
+            .body("errors[0].code", is("NotNull"))
+            .body(JsonSchemaValidator.matchesJsonSchema(readJsonFile("src/test/resources/schemas/ValidationErrorDTO.json")));
+    }
+
+    @Test
+    @DisplayName("POST /properties sending invalid mandatory attributes")
+    public void testCreatePropertySendingInvalidAttributes() throws IOException {
+        given()
+            .auth()
+            .preemptive()
+            .basic(username, password)
+            .body(readJsonFile("src/test/resources/requests/PropertyCreationSendingInvalidMandatoryAttribute.json"))
+        .when()
+            .post(POST_PROPERTIES_ENDPOINT)
+        .then()
+            .statusCode(400)
+            .body("title", is("Validation Error"))
+            .body("detail", is("Invalid attribute type"));
+    }
+
+
     //POST PROPERTIES: INVALID OPTIONAL ATTRIBUTES
     //POST PROPERTIES: VALID REQUEST WITHOUT OPTIONAL ATTRIBUTES
     //POST PROPERTIES: VALID REQUEST WITH OPTIONAL ATTRIBUTES
